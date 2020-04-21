505f25711a548596a502aa1beaa2e4ffa5c8c431
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-21 21:56:04 +0000

    fixed styling bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,13 @@ $(document).ready(function () {
   // --- our code goes here ---
   const tweetText = $('#tweet-text');
   tweetText.on('input', function () {
+    let chars = tweetText.val().length;
     const $counter = $(this.parentElement.querySelector('.prompt-footer').querySelector('.counter'));
-    $counter.text(140 - tweetText.val().length);
-    if (tweetText.val().length > 140) {
-      $counter.toggleClass('text-red');
+    $counter.text(140 - chars);
+    if (chars > 140) {
+      $counter.addClass('text-red');
+    } else {
+      $counter.removeClass('text-red');
     }
   });
 
