ac3f05730d5a6a4d9553dd80a3ee5f96887c9ad6
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-22 19:57:32 +0000

    AJAX GET and POST requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,3 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = (tweet) => {
   const now = Date.now();
   const daysAgo = Math.floor((now - tweet.created_at) / (1000 * 60 * 60 * 24));
@@ -51,15 +25,30 @@ const createTweetElement = (tweet) => {
   return markup;
 };
 
-
 $(document).ready(function () {
   const allTweets = $('.all-tweets');
   const renderTweets = function (tweets) {
     tweets.forEach(tweet => {
       let markup = createTweetElement(tweet);
-      allTweets.append(markup);
+      allTweets.prepend(markup);
     })
   }
-  renderTweets(data);
+
+  $('.post-tweet').submit(function (e) {
+    e.preventDefault();
+    const postTweet = $(this).serialize();
+    $.post('/tweets/', postTweet)
+      .then(() => {
+        console.log(postTweet)
+      })
+  });
+
+  const loadTweets = () => {
+    $.get('/tweets/')
+      .then((res) => {
+        renderTweets(res)
+      })
+  }
+  loadTweets();
 
 });
