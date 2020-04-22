1aabd18cd9401cc4de5e2e6c0989f599d50e7925
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-22 18:03:38 +0000

    cleaning up comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,7 @@ $(document).ready(function () {
   const tweetText = $('#tweet-text');
 
   tweetText.on('input', function () {
-    // Traverse Dom for counter
     const $counter = tweetText.siblings().last().children().last();
-
-    // Gettext area lenght
     let chars = tweetText.val().length;
     $counter.text(140 - chars);
     if (chars > 140) {
