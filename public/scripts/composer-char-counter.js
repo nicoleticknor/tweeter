3487a1bb6162c7e41a2250b0967e630bbbc49345
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-22 00:19:15 +0000

    refactored DOM traversal method

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-  // --- our code goes here ---
   const tweetText = $('#tweet-text');
+
   tweetText.on('input', function () {
+    // Traverse Dom for counter
+    const $counter = tweetText.siblings().last().children().last();
+
+    // Gettext area lenght
     let chars = tweetText.val().length;
-    const $counter = $(this.parentElement.querySelector('.prompt-footer').querySelector('.counter'));
     $counter.text(140 - chars);
     if (chars > 140) {
       $counter.addClass('text-red');
