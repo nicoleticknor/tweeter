d4ed410ec65d7f3c21c6c0e71ac55f0583ad00fa
Nicole Ticknor <ticknor.nicole@gmail.com>
2020-04-23 16:01:19 +0000

    nav button and new tweet styling added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,11 +4,26 @@ const escape = function (str) {
   return div.innerHTML;
 }
 
-// TODO adjust this func so it says months, days, years, hours, minutes, etc
 const createTweetElement = (tweet) => {
   const now = Date.now();
   const daysAgo = Math.floor((now - tweet.created_at) / (1000 * 60 * 60 * 24));
+  let time = null;
   // TODO add if statement here
+
+  if (daysAgo > 29 && daysAgo < 60) {
+    time = 'One month ago'
+  } else if (daysAgo >= 60 && daysAgo < 365) {
+    time = Math.floor(daysAgo / 12) + ' months ago';
+  } else if (daysAgo >= 365 && daysAgo < 730) {
+    time = 'Over a year ago'
+  } else if (daysAgo >= 730) {
+    time = Math.floor(daysAgo / 365) + ' years ago';
+  } else if ((daysAgo * 60) < 1) {
+    time = 'Less than an hour ago';
+  } else {
+    time = (daysAgo * 24) + ' hours ago';
+  }
+  // <span for="datestamp">${daysAgo} days ago</span>
 
   const markup = `<article>
       <header class="tweet-header">
@@ -22,7 +37,7 @@ const createTweetElement = (tweet) => {
         <p class="tweet-message">${escape(tweet.content.text)}</p>
       </div>
       <footer>
-        <span for="datestamp">${daysAgo} days ago</span>
+        <span for="datestamp">${time}</span>
         <span for="tweeter-actions">
           <i class="fontAwesomeIconSmall fas fa-flag"></i>
           <i class="fontAwesomeIconNull fas fa-flag"></i>
@@ -53,6 +68,18 @@ $(document).ready(function () {
   };
   loadTweets();
 
+  const $button = $('.new-tweet-btn');
+  const $newTwtSection = $('.new-tweet');
+
+  $button.click(() => {
+    if ($newTwtSection.is(':visible')) {
+      $newTwtSection.slideUp();
+    } else {
+      $newTwtSection.slideDown(); ("slow", () => { });
+      $('#tweet-text').focus();
+    }
+  })
+
   const $form = $('.post-tweet');
 
   $form.submit(function (e) {
@@ -69,16 +96,12 @@ $(document).ready(function () {
     $errorOver140.slideUp();
 
     if ($tweet.length === 0) {
-      $errorNoText.slideDown(); ("slow", () => {
-        $(self)[0].reset();
-      });
+      $errorNoText.slideDown(); ("slow", () => { });
       return;
     }
 
     if ($tweet.length > 140) {
-      $errorOver140.slideDown("slow", () => {
-        $(self)[0].reset();
-      });
+      $errorOver140.slideDown("slow", () => { });
       return;
     }
 
